d635baa14c74cc513c6e82e398bd6d5e4ef0cf20
Bheesetti Harsith Veera Charan <veeracharan99@gmail.com>
2025-05-19 16:31:00 +0530

    removed excess in p22.py

--- Patterns/p22.py
+++ Patterns/p22.py
@@ -27,36 +27,3 @@ print((str(n)+" ")*((n*2)-1))
 # 4 3 2 2 2 3 4
 # 4 3 3 3 3 3 4
 # 4 4 4 4 4 4 4
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# n=int(input())
-# for i in range(n,0,-1):
-#     for j in range(n,i-1,-1):
-#         print(j,end="")
-#     for k in range((i-1)*2):
-#         print(i,end="")
-#     for j in range(i,n+1):
-#         print(j,end="")
-#     print()
-# for i in range(n-1):
-#     num=n
-#     for j in range(n - i -1):
-#         print(num, end="")
-#         num -= 1
-#     val=i+2
-#     for k in range((i+1)*2):
-#         print(val,end="")
-    
-#     print()
