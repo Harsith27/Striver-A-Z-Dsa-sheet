698a7bb874377fe416764be13cfccc8acf85a051
Bheesetti Harsith Veera Charan <veeracharan99@gmail.com>
2025-10-06 10:59:52 +0530

    Count_Subbarray_with_given_sumK

---
+++ Arrays/Medium/Count_Subarrays_with_given_sum.py
@@ -0,0 +1,13 @@
+def countSubarrays(nums,k):
+    count=0
+    seen={0:1}
+    curr_sum=0
+    for num in nums:
+        curr_sum+=num
+        if curr_sum-k in seen:
+            count+=seen[curr_sum-k]
+        seen[curr_sum]=seen.get(curr_sum,0)+1
+    return count
+nums=[1,2,-1,1,-2,3,1,-2]
+k=4
+print(countSubarrays(nums,k))
